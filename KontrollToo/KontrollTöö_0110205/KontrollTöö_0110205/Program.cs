790d38213c7e25ebb33912d13ed4ffd6c07ffe8d
Leib92 <opilane@HP-CZC321705Q>
2025-10-10 14:40:06 +0300

    Kontrolltõõ 01.10.2025

---
+++ KontrollToo/KontrollTöö_0110205/KontrollTöö_0110205/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace KontrollTöö_0110205
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mis on teie nimi?");
+            string nimi = Console.ReadLine();
+
+            Console.WriteLine("Mis aasta te sündisite?");
+            int sünniaasta = int.Parse(Console.ReadLine());
+
+            int vanus = DateTime.Now.Year - sünniaasta;
+            if (vanus < 0)
+            {
+                Console.WriteLine("Te ei saa olla negatiivse vanasega.");
+            } else
+            {
+                Console.WriteLine($"Tere, {nimi}! Te olete {vanus} aastat vana.");
+            }
+
+
+            Console.WriteLine("Mis on praegu temperatuur?");
+            double temperatuur = double.Parse(Console.ReadLine());
+            if (temperatuur < 0)
+            {
+                Console.WriteLine("põrgu jaatus");
+            }
+            else if (temperatuur >= 0 && temperatuur <= 10)
+            {
+                Console.WriteLine("päris külm on");
+            }
+            else if (temperatuur >= 11 && temperatuur <= 20)
+            {
+                Console.WriteLine("Normaalne ilm");
+            }
+            else if (temperatuur >= 21 && temperatuur <= 30)
+            {
+                Console.WriteLine("Kes läheb grillimiseks?");
+            } 
+            else if (temperatuur >= 31 && temperatuur <= 40)
+            {
+                Console.WriteLine("APPI, GLOBAALJE SOOJENEMINE");
+            }
+
+            Console.WriteLine("Kas te tahate vaarikat, maasikat või apelsini?");
+            string valik = Console.ReadLine();
+            if (valik == "vaarikat") {
+                Console.BackgroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("näe, vaarikas!");
+            }
+            else if (valik == "maasikat")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("maasikaski on!");
+            } else if (valik == "apelsini")
+            {
+                Console.WriteLine("Apelsini mul kahjuks ei ole :С");
+            }
+        }
+    }
+}
